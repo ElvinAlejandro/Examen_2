798774e795d967a081392e2603ad29e54dc40644
Elvin Hernández <elvinh5@hotmail.com>
2021-11-13 01:37:51 -0600

    new

---
+++ Examen_2/Examen_2/Modelos/DAO/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_2.Modelos.DAO
+{
+    public class Conexion
+    {
+
+    }
+}
---
+++ Examen_2/Examen_2/Modelos/DAO/UsuarioDAO.cs
@@ -0,0 +1,43 @@
+﻿using Examen_2.Modelos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Examen_2.Modelos.DAO
+{
+    public class UsuarioDAO : Conexion
+    {
+        SqlCommand comando = new SqlCommand();
+
+
+
+        public bool ValidarUsuario(Usuario user)
+        {
+            bool valido = false;
+            try
+            {
+                StringBuilder sql = new StringBuilder();
+                sql.Append(" SELECT 1 FROM USUARIO WHERE CORREO = @Correo AND CLAVE = @Clave;");
+
+                comando.Connection = MiConexion;
+                MiConexion.Open();
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = sql.ToString();
+                comando.Parameters.Add("@Correo", SqlDbType.NVarChar, 30).Value = user.Correo;
+                comando.Parameters.Add("@Clave", SqlDbType.NVarChar, 30).Value = user.Clave;
+                valido = Convert.ToBoolean(comando.ExecuteScalar());
+                MiConexion.Close();
+            }
+            catch (Exception)
+            {
+            }
+            return valido;
+        }
+
+
+    }
+}
